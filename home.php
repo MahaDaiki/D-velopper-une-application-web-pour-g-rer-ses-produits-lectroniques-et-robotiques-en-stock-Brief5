39d5a28c6ea8fcf4d134427f857c994b0fb1b89f
MahaDaiki <142234887+MahaDaiki@users.noreply.github.com>
2023-11-23 16:00:42 +0100

    home.php

--- home.php
+++ home.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <nav>
         <nav class="navbar navbar-expand-sm navbar-dark">
             <div class="container">
                <a href="#" class="navbar-brand">NE</a>
